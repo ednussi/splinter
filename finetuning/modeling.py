c58c5c3572aa8327e3a416bc73cbdd000c87192f
Eran Nussinovitch <eran.nussinovitch@gmail.com>
2021-05-08 14:27:45 -0400

    reverting to older transformers library

--- finetuning/modeling.py
+++ finetuning/modeling.py
@@ -4,7 +4,8 @@ from torch.nn import Module, Linear, LayerNorm, CrossEntropyLoss
 from torch.nn.parameter import Parameter
 
 from transformers import BertPreTrainedModel, BertModel, RobertaModel
-from transformers.models.bert.modeling_bert import BertLMPredictionHead, ACT2FN
+# from transformers.models.bert.modeling_bert import BertLMPredictionHead, ACT2FN
+from transformers.modeling_bert import BertLMPredictionHead, ACT2FN
 
 
 def gather_positions(input_tensor, positions):
