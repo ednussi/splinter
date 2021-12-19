1e8d55f39db20e610fe03bfd6ff95f4548a6929f
Eran Nussinovitch <eran.nussinovitch@gmail.com>
2021-12-18 20:29:13 -0500

    update get qa results script

--- finetuning/get_results_dict.py
+++ finetuning/get_results_dict.py
@@ -4,8 +4,8 @@ import re
 import os
 import json
 import pandas as pd
-pd.set_option('display.max_rows', 500)
-pd.set_option('display.max_columns', 500)
+pd.set_option('display.max_rows', 800)
+pd.set_option('display.max_columns', 800)
 pd.set_option('display.width', 1000)
 
 def init_parser():
